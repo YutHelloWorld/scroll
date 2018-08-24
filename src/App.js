78f0349d3dc3a3db04c92d8db72443dd5f1f5664
Sven <sven@139.com>
2018-08-24 14:41:32 +0800

    ss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,103 @@
-import './App.css'
-
-import React, {Component} from 'react'
-
-class App extends Component {
-  render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-    </div>
-  }
+import React, { Component } from 'react'
+import BScroll from 'better-scroll'
+function Item({v, style}) {
+  const _style = { width: 100,height: 100, background: `hsl(${30 + 50*v},60%,50%)`, textAlign: 'center' }
+  return <div style={Object.assign(_style, style)}>item {v}</div>
 }
 
-export default App
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      arr: [1,2,3,4],
+      c: 0
+    }
+    this.el = React.createRef();
+  }
+  
+  
+
+  handleScroll = ({x, y}) => {
+    const itemCount = 4
+    const perHeight = 100;
+    const { c } = this.state
+    let n
+    if (y<0) {
+      n = parseInt((-y) / perHeight);
+      
+      if(n===2) {
+        if(!c) {
+          this.setState({
+            arr: [3,4,1,2],
+            c: 1
+          })
+          console.log('n---',n);
+        } else {
+          this.setState({
+            arr: [1,2,3,4],
+            c: 0
+          })
+          console.log('n---',n);
+        }
+        this.scroll.scrollTo(0,0)
+      }
+    } else {
+      n = parseInt((y) / perHeight);
+      if (!c) {
+        this.setState({
+          arr: [3,4,1,2],
+          c: 1
+        })
+      }else {
+        this.setState({
+          arr: [1,2,3,4],
+          c: 0
+        })
+      }
+      
+      this.scroll.scrollTo(0,-200)
+    }
+    
+  }
+  
+  
+  componentDidMount() {
+    this.scroll = new BScroll(this.el.current,{
+      probeType: 3,
+      click: true,
+      // bounce: false,
+      // momentum: false
+    })
+    this.scroll.on('scroll',this.handleScroll)
+  }
+  
+  
+  componentDidUpdate(prevProps, prevState) {
+    this.scroll = new BScroll(this.el.current,{
+      probeType: 3,
+      click: true,
+      // bounce: false,
+      // momentum: false
+    })
+    this.scroll.on('scroll',this.handleScroll)
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    return JSON.stringify(this.state.arr) !== JSON.stringify(nextState.arr)
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(this.state.translateYs);
+  }
+  
+  render() {
+    const { arr } = this.state
+    return (
+      <div style={{overflowY: 'auto', width: 100, height: 200, marginTop: 100, background: 'hsl(20, 50%, 90%)' }} ref={this.el}> 
+        <div>
+          {arr.map((v,i) => <Item v={v} key={v} />)}
+        </div>
+      </div>
+    )
+  }
+}
